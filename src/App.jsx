c61883c4758bf93924da4f88fd5ed47fca3c1c05
Abhishek De <deabhishek06@gmail.com>
2024-01-24 17:58:39 +0530

    Completed the UI part.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,68 @@
+import "./app.css";
+
 function App() {
   return (
-    <>
-      <h1 className="text-9xl bg-red-500">Hello world !</h1>
-    </>
+    <div className="grid grid-cols-fourlayout grid-rows-sixlayout justify-center mt-8">
+      <div className="bg-black/75 col-span-full flex flex-col justify-around items-end p-3 break-words break-all">
+        <div className="text-white/75 text-2xl">12,990</div>
+        <div className="text-white text-4xl">45,67,890</div>
+      </div>
+
+      <button className="bg-white/75 hover:bg-white/90 focus:bg-white/90 border border-white outline-none cursor-pointer text-3xl col-span-2">
+        AC
+      </button>
+      <button className="bg-white/75 hover:bg-white/90 focus:bg-white/90 border border-white outline-none cursor-pointer text-3xl">
+        DEL
+      </button>
+      <button className="bg-white/75 hover:bg-white/90 focus:bg-white/90 border border-white outline-none cursor-pointer text-3xl">
+        ÷
+      </button>
+      <button className="bg-white/75 hover:bg-white/90 focus:bg-white/90 border border-white outline-none cursor-pointer text-3xl">
+        1
+      </button>
+      <button className="bg-white/75 hover:bg-white/90 focus:bg-white/90 border border-white outline-none cursor-pointer text-3xl">
+        2
+      </button>
+      <button className="bg-white/75 hover:bg-white/90 focus:bg-white/90 border border-white outline-none cursor-pointer text-3xl">
+        3
+      </button>
+      <button className="bg-white/75 hover:bg-white/90 focus:bg-white/90 border border-white outline-none cursor-pointer text-3xl">
+        *
+      </button>
+      <button className="bg-white/75 hover:bg-white/90 focus:bg-white/90 border border-white outline-none cursor-pointer text-3xl">
+        4
+      </button>
+      <button className="bg-white/75 hover:bg-white/90 focus:bg-white/90 border border-white outline-none cursor-pointer text-3xl">
+        5
+      </button>
+      <button className="bg-white/75 hover:bg-white/90 focus:bg-white/90 border border-white outline-none cursor-pointer text-3xl">
+        6
+      </button>
+      <button className="bg-white/75 hover:bg-white/90 focus:bg-white/90 border border-white outline-none cursor-pointer text-3xl">
+        +
+      </button>
+      <button className="bg-white/75 hover:bg-white/90 focus:bg-white/90 border border-white outline-none cursor-pointer text-3xl">
+        7
+      </button>
+      <button className="bg-white/75 hover:bg-white/90 focus:bg-white/90 border border-white outline-none cursor-pointer text-3xl">
+        8
+      </button>
+      <button className="bg-white/75 hover:bg-white/90 focus:bg-white/90 border border-white outline-none cursor-pointer text-3xl">
+        9
+      </button>
+      <button className="bg-white/75 hover:bg-white/90 focus:bg-white/90 border border-white outline-none cursor-pointer text-3xl">
+        -
+      </button>
+      <button className="bg-white/75 hover:bg-white/90 focus:bg-white/90 border border-white outline-none cursor-pointer text-3xl">
+        .
+      </button>
+      <button className="bg-white/75 hover:bg-white/90 focus:bg-white/90 border border-white outline-none cursor-pointer text-3xl">
+        0
+      </button>
+      <button className="bg-white/75 hover:bg-white/90 focus:bg-white/90 border border-white outline-none cursor-pointer text-3xl col-span-2">
+        =
+      </button>
+    </div>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateColumns: {
+        fourlayout: " repeat(4, 6rem)",
+      },
+      gridTemplateRows: {
+        sixlayout: "minmax(7rem, auto) repeat(5, 6rem)",
+      },
+    },
   },
   plugins: [],
 };
